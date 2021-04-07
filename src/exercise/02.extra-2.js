f1c6155502ae2e455c34a75483e1bca1ac00043d
Felipe Lee <felipe@truss.works>
2021-04-07 17:08:59 -0500

    Refactor component w/useEffect to use a custom hook for extra credit 3

--- src/exercise/02.extra-2.js
+++ src/exercise/02.extra-2.js
@@ -3,25 +3,18 @@
 
 import * as React from 'react'
 
-const useLocalStorageState = (key, defaultValue = '') => {
-  const [state, setState] = React.useState(
-    () => window.localStorage.getItem(key) || defaultValue,
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(
+    () => window.localStorage.getItem('name') || initialName,
   )
 
   React.useEffect(() => {
-    window.localStorage.setItem(key, state)
-  }, [key, state])
-
-  return [state, setState]
-}
-
-function Greeting({initialName = ''}) {
-  const [name, setName] = useLocalStorageState('name', initialName)
+    window.localStorage.setItem('name', name)
+  }, [name])
 
   function handleChange(event) {
     setName(event.target.value)
   }
-
   return (
     <div>
       <form>
