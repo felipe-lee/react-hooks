2858cebfe8f4f39e2850d23d5235a03ab458144e
Felipe Lee <felipe@truss.works>
2021-04-07 18:01:43 -0500

    Update useLocalStorageState to handle any data type
    It can also now gracefully handle changes to the key being used.

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,32 +3,64 @@
 
 import * as React from 'react'
 
-const useLocalStorageState = (key, defaultValue = '') => {
-  const [state, setState] = React.useState(
-    () => window.localStorage.getItem(key) || defaultValue,
-  )
+const useLocalStorageState = (
+  key,
+  defaultValue,
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) => {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = React.useRef(key)
 
   React.useEffect(() => {
-    window.localStorage.setItem(key, state)
-  }, [key, state])
+    const prevKey = prevKeyRef.current
+
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+
+    prevKeyRef.current = key
+
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state])
 
   return [state, setState]
 }
 
 function Greeting({initialName = ''}) {
-  const [name, setName] = useLocalStorageState('name', initialName)
+  const [person, setPerson] = useLocalStorageState('person', {
+    name: initialName,
+  })
 
   function handleChange(event) {
-    setName(event.target.value)
+    console.log(event)
+    const updatedPerson = {...person, [event.target.id]: event.target.value}
+
+    setPerson(updatedPerson)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={person.name} onChange={handleChange} id="name" />
+        <br />
+        <label htmlFor="email">Email: </label>
+        <input value={person.email} onChange={handleChange} id="email" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {person.name ? (
+        <strong>Hello {person.name}</strong>
+      ) : (
+        'Please type your name'
+      )}
     </div>
   )
 }
