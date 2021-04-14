49d84ed33dd82c9bda8c5979ea23aa3a45e2f4d5
Felipe Lee <felipe@truss.works>
2021-04-14 17:29:14 -0500

    Update to keep name state in Name component

--- src/exercise/03.original.js
+++ src/exercise/03.original.js
@@ -3,17 +3,11 @@
 
 import * as React from 'react'
 
-function Name() {
-  const [name, setName] = React.useState('')
-
+function Name({name, onNameChange}) {
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input
-        id="name"
-        value={name}
-        onChange={event => setName(event.target.value)}
-      />
+      <input id="name" value={name} onChange={onNameChange} />
     </div>
   )
 }
@@ -27,23 +21,24 @@ function FavoriteAnimal({animal, onAnimalChange}) {
   )
 }
 
-function Display({animal}) {
-  return <div>{`Your favorite animal is: ${animal}!`}</div>
+function Display({name, animal}) {
+  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
   const [animal, setAnimal] = React.useState('')
+  const [name, setName] = React.useState('')
 
   return (
     <form>
-      <Name />
+      <Name name={name} onNameChange={event => setName(event.target.value)} />
 
       <FavoriteAnimal
         animal={animal}
         onAnimalChange={event => setAnimal(event.target.value)}
       />
 
-      <Display animal={animal} />
+      <Display name={name} animal={animal} />
     </form>
   )
 }
